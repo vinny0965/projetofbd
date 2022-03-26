7d94141e950a0c1a91314fd2ab66bf6aafdc8ca4
Vinicius Carvalho <carvalho.vinicius163@gmail.com>
2022-03-26 19:16:07 -0300

    criando entidades administrador e cliente

--- src/main/java/br/com/barbeariaFurao/datasouce/model/Endereco.java
+++ src/main/java/br/com/barbeariaFurao/datasouce/model/Endereco.java
@@ -1,11 +1,14 @@
 package br.com.barbeariaFurao.datasouce.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -26,6 +29,12 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private Integer numero;
 	private String cep;
+	
+	@OneToMany(mappedBy = "endereco")
+	private List<Administrador>administradores = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "endereco")
+	private List<Cliente>clientes = new ArrayList<>();
 	
 	
 	@SuppressWarnings("unused")
